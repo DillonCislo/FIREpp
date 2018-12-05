1f3dbffa6fee6f2d213c6c66560fab9f30450031
DillonCislo <dilloncislo@gmail.com>
2018-12-05 15:32:03 -0800

    Added src licences and updated documentation

--- FIRE/MolecularDynamics.h
+++ FIRE/MolecularDynamics.h
@@ -1,11 +1,32 @@
-/*!
+/*********************************************************************************************** 
+ * Copyright (C) 2018 Dillon Cislo
+ *
+ * This file is part of FIRE++.
+ *
+ * FIRE++ is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will by useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>
+ *
+ ***********************************************************************************************/
+
+/*********************************************************************************************//**
  * 	\file MolecularDynamics.h
  * 	\brief Molecular dynamics integrators to find updated particle positions.
  *
+ * 	I should write a detailed explanation at some point.
+ *
  * 	\author Dillon Cislo
  * 	\date 11/16/2018
+ * 	\copyright GNU Public License
  *
- */
+ ************************************************************************************************/
 
 #ifndef _MOLECULAR_DYNAMICS_H_
 #define _MOLECULAR_DYNAMICS_H_
